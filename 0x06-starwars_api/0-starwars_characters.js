8df954a743dcfe81287f9310bf4617034eaeca39
Tesfay-Teshome <tesfix2017@gmail.com>
2023-03-09 19:45:40 +0300

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const API_URL = 'https://swapi-api.alx-tools.com/api';
+const API_URL = 'https://swapi-api.hbtn.io/api/';
 
 if (process.argv.length > 2) {
-  request('${API_URL}/films/${process.argv[2]}/', (err, _, body) => {
+  request('${API_URL}/films/${process.argv[3]}/', (err, _, body) => {
     if (err) {
       console.log(err);
     }
